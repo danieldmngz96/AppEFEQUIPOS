683876cefda899428e00c0025cb75b855ea6a030
Daniel Dominguez Dominguez <danarturodo@hotmail.com>
2023-03-14 20:01:40 -0500

    Aplicacion control inventario EFQUIPOS
    PRIMER  COMMIT DE EFEQUIPOS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './module/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/',
+        pathMatch: 'full',
+      },
+    ],
+  },
+/*   {
+    path: 'Activity',
+    loadChildren: () => import('./modules/actividades/actividades.module').then(m => m.ActividadesModule)
+  },
+  {
+    path: 'bienvenido',
+    loadChildren: () => import('./modules/landing/landing.module').then(m => m.LandingModule)
+  },
+  {
+    path: 'cultivos',
+    loadChildren: () => import('./modules/cultivos/cultivos.module').then(m => m.CultivosModule)
+  },
+  {
+    path: 'reportes',
+    loadChildren: () => import('./modules/reportes/reportes.module').then(m => m.ReportesModule)
+  }, */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
