1d0d192bd1283345f4b659e25cd573a20973c7df
Daniel Dominguez Dominguez <danarturodo@hotmail.com>
2023-09-06 23:25:28 -0500

    ajuste en servicios bd_efequipos funciona y almacen

--- src/app/services/clientes.service.ts
+++ src/app/services/clientes.service.ts
@@ -1,13 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const apiUrl = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
 
-  api = "http://localhost:3000"
+  //api = "http://localhost:3000"
+  api = apiUrl;
   constructor(private http: HttpClient) { }
   /**
    * @author Daniel Dominguez
