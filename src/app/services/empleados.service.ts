24dda96d4d7336911e5f8e0c62e18b46dd86e53d
Daniel Dominguez Dominguez <danarturodo@hotmail.com>
2023-09-06 21:57:54 -0500

    add url api

--- src/app/services/empleados.service.ts
+++ src/app/services/empleados.service.ts
@@ -1,13 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const apiUrl = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpleadosService {
 
-  api = "http://localhost:3000/almacen"
+  api = apiUrl+'/almacen';
 
   constructor(private http: HttpClient) { }
   /**
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,13 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const apiUrl = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  api = "http://localhost:3000/bd_efequipos"
+  api = apiUrl+"/bd_efequipos";
   constructor(private http: HttpClient) { }
   /**
   * @author Daniel Dominguez
