33f044adb2ed0997f276962b46edb3ace1948ee6
Daniel Dominguez Dominguez <danarturodo@hotmail.com>
2023-09-21 20:43:16 -0500

    modulo de pqrs

--- src/app/modules/pqrs/pqrs.component.ts
+++ src/app/modules/pqrs/pqrs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-pqrs',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pqrs.component.scss']
 })
 export class PqrsComponent implements OnInit {
-
+  pqrsForm!: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.pqrsForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      direccion: new FormControl('', [Validators.required]), 
+      phone: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      contry: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]), 
+      pqrs: new FormControl('', [Validators.required]), 
+    });
   }
 
 }
